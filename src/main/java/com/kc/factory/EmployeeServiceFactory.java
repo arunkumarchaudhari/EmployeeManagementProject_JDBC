cfbfe2eaa2b6ee5e0dff56e042ea7bb5befd9233
Arun Kumar Chaudhari <your-email@example.com>
2026-02-11 22:15:06 +0530

    Employee Management Project

---
+++ src/main/java/com/kc/factory/EmployeeServiceFactory.java
@@ -0,0 +1,14 @@
+package com.kc.factory;
+
+import com.kc.service.EmployeeSearviceImpl;
+import com.kc.service.EmployeeService;
+
+public class EmployeeServiceFactory {
+    private static EmployeeService employeeService;
+    static{
+        employeeService = new EmployeeSearviceImpl();
+    }
+    public static EmployeeService getEmployeeService(){
+        return employeeService;
+    }
+}
